7400688bb08cc1d0e6f37433653e24ef3f5e8f40
AldoRed <aldo@aldored.com>
2025-04-06 01:39:13 -0400

    Add math rendering support with KaTeX and remark-math

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -41,6 +43,8 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/AldoRed-Software/DocsAldoRed/tree/main/docs",
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
         },
         blog: {
           showReadingTime: true,
